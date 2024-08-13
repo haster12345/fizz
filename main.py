c3db1429dff05f265ef71600665183bbb63a2357
Kane Kerr <Kanekerr6@mail.com>
2024-08-13 16:16:59 +0100

    Added key movement

--- main.py
+++ main.py
@@ -1,53 +1,47 @@
 import time
 import os
+import keyboard
 
 FPS = 30
 LEFT_BOUND = 0
 RIGHT_BOUND = 30
 UP_BOUND = 0
 DOWN_BOUND = 30
-TIME_TO_MOVE = 10
+TIME_TO_MOVE = 1  # every interation we move/ ie if this was 30 every 30 iterations we would move
 
 
 def main():
     refresh_counter = 0
-    x_move_counter = 0
-    hit_left = True
-    hit_right = False
-    hit_bottom = False
-    hit_top = True
-    y_move_counter = 0
+    move_counter = 0
+    moving_left = False
+    moving_right = False
 
     while True:
-        while not hit_right:
-            refresh_counter += 1
-            if refresh_counter % TIME_TO_MOVE == 0:
-                x_move_counter += 1
-                y_move_counter += 1
-            particle(x_move_counter, y_move_counter)
-            refresh()
-            if x_move_counter == RIGHT_BOUND:
-                hit_right = True
-                hit_left = False
-                hit_bottom = True
-                hit_top = False
+        if keyboard.is_pressed("d"):
+            moving_right = True
+            moving_left = False
+        elif keyboard.is_pressed("a"):
+            moving_left = True
+            moving_right = False
 
-        while not hit_left:
-            refresh_counter += 1
-            if refresh_counter % TIME_TO_MOVE == 0:
-                x_move_counter -= 1
-                y_move_counter -= 1
-            particle(x_move_counter, y_move_counter)
-            refresh()
-            if x_move_counter == 0:
-                hit_right = False
-                hit_left = True
-                hit_top = True
-                hit_bot = False
+        refresh_counter += 1
+        if refresh_counter % TIME_TO_MOVE == 0:
+            if moving_right:
+                move_counter += 1
+            elif moving_left:
+                move_counter -= 1
+
+        move(move_counter)
+        refresh()
 
 
-def particle(x, y):
-    print('\n' * y, ' ' * x, '.')
+# Lines below allow it to move on its own
+        # if move_counter >= RIGHT_BOUND:
+        #     moving_right = False
+        #     hit_left = True
+        # elif move_counter <= LEFT_BOUND:
+        #     moving_right = True
+        #     hit_left = False
 
 
 def refresh():
@@ -56,17 +50,13 @@ def refresh():
     and we want 30 fps so that means we want 1 frame every 33 milliseconds
     """
     time.sleep(FPS/1000)
-    os.system('clear')
+    os.system('cls')
     return
 
 
 def move(left_move_counter):
-    print(' ' * left_move_counter,  '.' )
+    print(' ' * left_move_counter,  '.')
     return
-
-
-def move_y(y_move_counter):
-    print('\n'* y_move_counter, '.')
 
 
 if __name__ == '__main__':
